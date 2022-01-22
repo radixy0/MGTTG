8d1f1832d612b610f4a4c67b80dba2bea8033170
radixy0 <ernst.lukas@gmx.net>
2022-01-22 17:38:27 +0100

    implemented methods in MainController class

--- src/main/java/com/itemis/mgttg/controller/MainController.java
+++ src/main/java/com/itemis/mgttg/controller/MainController.java
@@ -1,12 +1,13 @@
 package com.itemis.mgttg.controller;
 import com.itemis.mgttg.model.*;
+import com.itemis.mgttg.exceptions.*;
 
 public class MainController {
-    Minerals minerals;
+    Materials materials;
     Words words;
     private static MainController maincontroller_instance = null;
     public MainController(){
-        minerals = new Minerals();
+        materials = new Materials();
         words = new Words();
     }
     public static MainController getInstance(){
@@ -15,28 +16,95 @@ public class MainController {
         }
         return maincontroller_instance;
     }
-    public int getMineralPrice(String mineral){
-        return 0;
+
+    /**
+     * Gets the Price for a mineral String as float
+     * @param mineral the mineral to look up
+     * @return mineral price as float, null if unknown
+     */
+    public float getMineralPrice(String mineral){
+        return materials.getPrice(mineral);
     }
+
+    /**
+     * Gets the Roman Character for a word
+     * @param word the word to look up
+     * @return Roman value as Character, null if unknown
+     */
     public Character getWordsRomanValue(String word){
-        return null;
+        return words.getValue(word);
     }
-    public void addMineral(){
 
+    /**
+     * Adds a new mineral and its price to the Model
+     * @param mineral Mineral String to add
+     * @param value Price
+     * @throws MaterialPriceException if the Mineral is already known with a different price
+     */
+    public void addMineral (String mineral, float value) throws MaterialPriceException {
+        materials.addMaterial(mineral, value);
     }
-    public void addWord(){
 
+    /**
+     * Adds a new mineral and its price to the Model
+     * @param mineral Mineral String to add
+     * @param value Price
+     * @throws MaterialPriceException if the Mineral is already known with a different price
+     */
+    public void addMineral(String mineral, int value) throws MaterialPriceException {
+        addMineral(mineral, (float) value);
     }
-    public void updateMineral(){
 
+    /**
+     * Adds a new word and its roman value to the model
+     * @param word Word to add
+     * @param romanValue Value
+     * @throws WordAlreadyExistsException if the word already exists for a different roman Letter
+     */
+    public void addWord(String word, char romanValue) throws WordAlreadyExistsException{
+        words.addWord(word, romanValue);
     }
-    public void updateWord(){
 
+    /**
+     * Updates price for an already known mineral
+     * @param mineral Mineral to update
+     * @param value  Value
+     */
+    public void updateMineral(String mineral, float value){
+        materials.updatePrice(mineral, value);
     }
-    public void removeMineral(){
 
+    /**
+     * Updates price for an already known mineral
+     * @param mineral Mineral to update
+     * @param value  Value
+     */
+    public void updateMineral(String mineral, int value){
+        updateMineral(mineral, (float) value);
     }
-    public void removeWord(){
 
+    /**
+     * Updates value of an already known word
+     * @param word Word to update
+     * @param romanValue value
+     */
+    public void updateWord(String word, char romanValue){
+        words.updateWord(word, romanValue);
+    }
+
+    /**
+     * Removes a Mineral from model
+     * @param mineral Mineral to remove
+     */
+    public void removeMineral(String mineral){
+        materials.removeMaterial(mineral);
+    }
+
+    /**
+     * Removes a Word from model
+     * @param word Word to remove
+     */
+    public void removeWord(String word){
+        words.removeWord(word);
     }
 }
