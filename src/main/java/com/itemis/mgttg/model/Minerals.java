0d62c251071a069cde339511facfd204481dd235
radixy0 <ernst.lukas@gmx.net>
2022-01-22 17:00:42 +0100

    Added javadoc comments in model classes

--- src/main/java/com/itemis/mgttg/model/Minerals.java
+++ src/main/java/com/itemis/mgttg/model/Minerals.java
@@ -1,24 +1,52 @@
 package com.itemis.mgttg.model;
 import java.util.HashMap;
+import java.util.Objects;
+
 import com.itemis.mgttg.exceptions.MineralPriceException;
 //TODO comment for doc
 public class Minerals {
+
     HashMap<String, Float> minerals;
+
     public Minerals(){
         minerals = new HashMap<>();
     }
+
+    /**
+     * Adds a new Mineral to internal HashMap
+     * @param mineral Mineral to add
+     * @param value Value of the Mineral
+     * @throws MineralPriceException if the Mineral is already known with a different value
+     */
     public void addMineral(String mineral, Float value) throws MineralPriceException{
-        if(minerals.containsKey(mineral) && minerals.get(mineral) != value){
+        if(minerals.containsKey(mineral) && !Objects.equals(minerals.get(mineral), value)){
             throw new MineralPriceException("The Mineral " + mineral + " already seems to have a different Value of "+value+".", mineral, value);
         }
         minerals.put(mineral, value);
     }
+
+    /**
+     * Removes Mineral from the internal HashMap
+     * @param mineral Mineral to remove
+     */
     public void removeMineral(String mineral){
         minerals.remove(mineral);
     }
+
+    /**
+     * Updates Price of a Mineral
+     * @param mineral Mineral to Update
+     * @param value New Price
+     */
     public void updatePrice(String mineral, float value){
         minerals.put(mineral, value);
     }
+
+    /**
+     * Gets Price of a Mineral
+     * @param mineral
+     * @return Price as float, null if the mineral is unknown
+     */
     public float getPrice(String mineral){
         return minerals.get(mineral);
     }
--- src/main/java/com/itemis/mgttg/model/Words.java
+++ src/main/java/com/itemis/mgttg/model/Words.java
@@ -3,22 +3,47 @@ import java.util.HashMap;
 import com.itemis.mgttg.exceptions.WordAlreadyExistsException;
 //TODO Comment for doc
 public class Words {
+
     HashMap<String, Character> words;
     public Words(){
         words = new HashMap<>();
     }
+
+    /**
+     * Adds a new Word and its roman Character to internal HashMap
+     * @param word Word to add
+     * @param value roman Character
+     * @throws WordAlreadyExistsException
+     */
     public void addWord(String word, char value) throws WordAlreadyExistsException{
         if(words.containsKey(word) && words.get(word) != value){
             throw new WordAlreadyExistsException("I already know this word as " + words.get(word), word, words.get(word));
         }
         words.put(word, value);
     }
+
+    /**
+     * Removes a Word from internal HashMap
+     * @param word Word to remove
+     */
     public void removeWord(String word){
         words.remove(word);
     }
+
+    /**
+     * Updates an already known words roman Value
+     * @param word Word to update
+     * @param value new value
+     */
     public void updateWord(String word, char value){
         words.put(word, value);
     }
+
+    /**
+     * gets Value of a known word
+     * @param word word to look up
+     * @return the words value as float, null if the word is unknown
+     */
     public char getValue(String word){
         return words.get(word);
     }
